76933aa707fd87e00492c70ae16608d401b591e4
Ignacio de Jesús Acosta Contreras <ignacio.jesus.acosta.contreras@gmail.com>
2020-11-14 19:02:43 +0000

    Ejemplo de RStudio Cloud Session 4

---
+++ Sesion 4.R
@@ -0,0 +1,19 @@
+archivo<- list.files()
+lista <- lapply(archivos, read.cvs)
+h1 <- head(lista[[1]])
+h2 <- head(list[[2]])
+h1:h2
+do.call(rbind, list(h1, h2))
+datos <- do.call(rbind, lista)
+
+
+install.packages ("dplyr")
+library(dplyr)
+dir()
+X <- lapply(dir(), read.csv)
+X [[3]]
+Y <- do.call(rbind, X)
+View (Y)
+write.csv (Y, "Data.csv")
+dir()
+
